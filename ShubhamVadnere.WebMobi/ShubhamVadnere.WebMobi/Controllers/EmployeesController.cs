22e62fb582d725beedfc42f01ae71bba6f577ed1
sai <sai@DESKTOP-5POO3CH>
2021-07-02 19:41:00 +0530

    push all files

---
+++ ShubhamVadnere.WebMobi/ShubhamVadnere.WebMobi/Controllers/EmployeesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShubhamVadnere.WebMobi.EFDataModel;
+
+namespace ShubhamVadnere.WebMobi.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+
+        // GET: Employees
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Employees.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "Name");
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+
+        // GET: Employees/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "Name", employee.DepartmentId);
+
+            return View(employee);
+        }
+                
+        [HttpPost]
+        public async Task<ActionResult> Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "Name", employee.DepartmentId);
+
+            return View(employee);
+        }
+
+
+        // GET: Employees/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ShubhamVadnere.WebMobi/ShubhamVadnere.WebMobi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShubhamVadnere.WebMobi.MvcApplication" Language="C#" %>
